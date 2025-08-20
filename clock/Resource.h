3bce4dfbe0296714e6af69d1974cda35c9fe1171
Skor <40518486+Skordima@users.noreply.github.com>
2025-08-20 12:42:48 +0300

    Translate program to Russian

--- clock/Resource.h
+++ clock/Resource.h
@@ -1,6 +1,6 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Archivo de inclusión creado en Microsoft Visual C++.
-// Se utiliza el archivo de recursos resource.rc
+// Файл включения, созданный в Microsoft Visual C++.
+// Используется файл ресурсов resource.rc
 
 #define IDS_APP_TITLE       103
 
@@ -8,16 +8,16 @@
 #define IDD_ABOUTBOX        103
 #define IDM_ABOUT           104
 #define IDM_EXIT            105
-#define IDC_ABOUT           106   // Nuevo botón "Acerca de la aplicación"
-#define IDI_CLOCK           107      // Su ícono de la aplicación
-#define IDI_SMALL           108      // Su ícono pequeño
+#define IDC_ABOUT           106   // Новая кнопка «О программе»
+#define IDI_CLOCK           107      // Иконка приложения
+#define IDI_SMALL           108      // Маленькая иконка
 #define IDC_CLOCK           109
 #define IDC_MYICON          2
 #ifndef IDC_STATIC
 #define IDC_STATIC          -1
 #endif
 
-// Valores estándar para nuevos objetos
+// Стандартные значения для новых объектов
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC         130
@@ -35,6 +35,6 @@
 #define IDD_CUSTOM_ABOUTBOX    110
 #define IDC_GAMECLOCK 111
 
-// Recurso de notificación (archivo WAV)
-// Reemplace "gamemus.wav" por el nombre de su archivo (o use otro tipo de recurso)
+// Ресурс уведомления (файл WAV)
+// Замените "gamemus.wav" на имя вашего файла (или используйте другой тип ресурса)
 #define IDR_NOTIFICATION    130
